4728002e3a89b277db52a5abed15fe14e49bbaf0
Kyle Nakamura <knakamura13@apu.edu>
2018-05-27 01:42:47 -0700

    Initial Commit

---
+++ Bubble Wrap 2.0/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Bubble Wrap 2.0
+//
+//  Created by Kyle Nakamura on 5/27/18.
+//  Copyright © 2018 Kyle Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
