fa5fc617ec5f1266636545d6a42110416083c0ac
Kyle Nakamura <knakamura13@apu.edu>
2018-06-13 22:48:51 -0700

    Finish keyboard dismissal Create Item, partially unfinished for DescriptionTextView

--- Bubble Wrap 2.0/Controllers/CreateItemVC.swift
+++ Bubble Wrap 2.0/Controllers/CreateItemVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class CreateItemVC: UIViewController {
+class CreateItemVC: UIViewController, UITextFieldDelegate, UITextViewDelegate {
     
     // Constants
     
@@ -21,13 +21,19 @@ class CreateItemVC: UIViewController {
     @IBOutlet weak var smallImg1: UIImageView!
     @IBOutlet weak var smallImg2: UIImageView!
     @IBOutlet weak var smallImg3: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleTextField.delegate = self
+        priceTextField.delegate = self
+        descriptionTextView.delegate = self
         
         // Customize the visuals
         navigationController?.navigationBar.barTintColor = Constants.Colors.appPrimaryColor
-        scrollView.contentSize.height = 750 // arbitrary integer; increase if content does not fit in contentSize
+        scrollView.contentSize.height = 1000 // arbitrary integer; increase if content does not fit in contentSize
         let cornerRadius = CGFloat(10)
         smallImg1.layer.cornerRadius = cornerRadius
         smallImg2.layer.cornerRadius = cornerRadius
@@ -36,6 +42,20 @@ class CreateItemVC: UIViewController {
         smallImg1.image = demoPicsumImages.randomElement()
         smallImg2.image = demoPicsumImages.randomElement()
         smallImg3.image = demoPicsumImages.randomElement()
+        
+        self.hideKeyboardWhenTappedAround() // Hide keyboard on background tap
+    }
+    
+    // Jump from usernameField to passwordField, then hide the keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == titleTextField {
+            priceTextField.becomeFirstResponder()
+        } else if textField == priceTextField {
+            descriptionTextView.becomeFirstResponder()
+        } else {
+            resignFirstResponder()
+        }
+        return true
     }
     
     override func viewWillAppear(_ animated: Bool) {
