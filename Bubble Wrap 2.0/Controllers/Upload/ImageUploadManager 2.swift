e8d49f2bc8024150e75724e7f24139d645e64a47
Kyle Nakamura <knakamura13@apu.edu>
2018-06-23 12:17:45 -0700

    Add Item object, other changes

---
+++ Bubble Wrap 2.0/Controllers/Upload/ImageUploadManager 2.swift
@@ -0,0 +1,49 @@
+//
+//  ImageUploadManager.swift
+//  Bubble Wrap 2.0
+//
+//  Created by Kyle Nakamura on 6/15/18.
+//  Copyright © 2018 Kyle Nakamura. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseFirestore
+
+class ImageUploadManager: NSObject {
+    
+    func uploadImage(_ image: UIImage, progressBlock: @escaping (_ percentage: Float) -> Void, completionBlock: @escaping (_ url: URL?, _ errorMessage: String?) -> Void) {
+        let storageReference = Storage.storage().reference()
+        
+        let imageName = "\(Date().timeIntervalSince1970).jpg"
+        let imageReference = storageReference.child("itemImages").child(imageName)
+        
+        if let imageData = UIImageJPEGRepresentation(image, 0.8) {
+            let metadata = StorageMetadata()
+            metadata.contentType = "image/jpeg"
+            
+            let uploadTask = imageReference.putData(imageData, metadata: metadata, completion: { (metadata, error) in
+                if let _ = metadata {
+                    imageReference.downloadURL { (url, error) in
+                        if let downloadURL = url {
+                            completionBlock(downloadURL, nil)
+                        }
+                    }
+                } else {
+                    completionBlock(nil, error?.localizedDescription)
+                }
+            })
+            uploadTask.observe(.progress, handler: { (snapshot) in
+                guard let progress = snapshot.progress else {
+                    return
+                }
+                
+                let percentage = (Float(progress.completedUnitCount) / Float(progress.totalUnitCount)) * 100
+                progressBlock(percentage)
+            })
+        } else {
+            completionBlock(nil, "Image couldn't be converted to Data.")
+        }
+    }
+    
+}
