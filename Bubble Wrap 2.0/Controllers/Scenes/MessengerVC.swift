4d75507bdd02582f666c57d3692dfdc75f67c65b
Kyle Nakamura <knakamura13@apu.edu>
2018-06-30 17:58:41 -0700

    Successfully add three imageView bubbles to chat screen using bottom anchors attached to previous bubble's top anchor

--- Bubble Wrap 2.0/Controllers/Scenes/MessengerVC.swift
+++ Bubble Wrap 2.0/Controllers/Scenes/MessengerVC.swift
@@ -16,23 +16,68 @@ class MessengerVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var image: UIImage = UIImage(named: "chat_bubble_sent")!
-        var imageView = UIImageView(image: image)
-        self.view.addSubview(imageView)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
+//        let image: UIImage = UIImage(named: "chat_bubble_sent")!
+//        let imageView = UIImageView(image: image)
+//        self.view.addSubview(imageView)
+//        imageView.translatesAutoresizingMaskIntoConstraints = false
+//
+////        let topConstraint = NSLayoutConstraint(item: imageView, attribute: .top, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .top, multiplier: 1, constant: 50)
+//        let rightConstraint = NSLayoutConstraint(item: imageView, attribute: .trailing, relatedBy: .equal, toItem: imageView.superview, attribute: .trailing, multiplier: 1, constant: -25)
+//        let bottomConstraint = NSLayoutConstraint(item: imageView, attribute: .bottom, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .bottom, multiplier: 1, constant: -25)
+//        view.addConstraints([bottomConstraint, rightConstraint])
+//
+//        let heightConstraint = NSLayoutConstraint(item: imageView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 50)
+//        let widthConstraint = NSLayoutConstraint(item: imageView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 200)
+//        heightConstraint.constant = CGFloat.random(in: 45 ... 70)
+//        widthConstraint.constant = CGFloat.random(in: 180 ... 220)
+//        imageView.addConstraints([heightConstraint, widthConstraint])
         
-        let topConstraint = NSLayoutConstraint(item: imageView, attribute: .top, relatedBy: .equal, toItem: self.topLayoutGuide, attribute: .bottom, multiplier: 1, constant: 50)
-        let heightConstraint = NSLayoutConstraint(item: imageView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 50)
-        let widthConstraint = NSLayoutConstraint(item: imageView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 200)
-        
-        topConstraint.constant = CGFloat.random(in: 20 ... 100)
-        view.addConstraint(topConstraint)
-        imageView.addConstraints([heightConstraint, widthConstraint])
+        createSentMessage()
     }
     
     func createSentMessage() {
-        changeImage("chat_bubble_sent")
-        bubbleImageView.tintColor = UIColor(named: "chat_bubble_color_sent")
+//        changeImage("chat_bubble_sent")
+//        bubbleImageView.tintColor = UIColor(named: "chat_bubble_color_sent")
+        
+        let image: UIImage = UIImage(named: "chat_bubble_sent")!
+        var prevImgView: UIImageView!
+        for i in 1 ... 3 {
+            if i == 1 {
+                let newImgView = UIImageView(image: image)
+                
+                self.view.addSubview(newImgView)
+                newImgView.translatesAutoresizingMaskIntoConstraints = false
+                
+                let rightConstraint = NSLayoutConstraint(item: newImgView, attribute: .trailing, relatedBy: .equal, toItem: newImgView.superview, attribute: .trailing, multiplier: 1, constant: -25)
+                let bottomConstraint = NSLayoutConstraint(item: newImgView, attribute: .bottom, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .bottom, multiplier: 1, constant: -25)
+                view.addConstraints([bottomConstraint, rightConstraint])
+                
+                let heightConstraint = NSLayoutConstraint(item: newImgView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 50)
+                let widthConstraint = NSLayoutConstraint(item: newImgView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 200)
+                heightConstraint.constant = CGFloat.random(in: 45 ... 70)
+                widthConstraint.constant = CGFloat.random(in: 180 ... 220)
+                newImgView.addConstraints([heightConstraint, widthConstraint])
+                
+                prevImgView = newImgView
+            } else {
+                let newImgView = UIImageView(image: image)
+                
+                self.view.addSubview(newImgView)
+                newImgView.translatesAutoresizingMaskIntoConstraints = false
+                
+                let rightConstraint = NSLayoutConstraint(item: newImgView, attribute: .trailing, relatedBy: .equal, toItem: newImgView.superview, attribute: .trailing, multiplier: 1, constant: -25)
+                newImgView.bottomAnchor.constraint(equalTo: prevImgView.topAnchor, constant: -20.0).isActive = true
+                view.addConstraints([rightConstraint])
+                
+                let heightConstraint = NSLayoutConstraint(item: newImgView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 50)
+                let widthConstraint = NSLayoutConstraint(item: newImgView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 200)
+                heightConstraint.constant = CGFloat.random(in: 45 ... 70)
+                widthConstraint.constant = CGFloat.random(in: 180 ... 220)
+                newImgView.addConstraints([heightConstraint, widthConstraint])
+                
+                prevImgView = newImgView
+            }
+        }
     }
     
     func createReceivedMessage() {
