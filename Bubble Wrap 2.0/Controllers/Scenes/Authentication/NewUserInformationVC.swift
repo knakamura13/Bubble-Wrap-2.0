b150f81ee95b03166748aa491174a1e746f47aa4
Kyle Nakamura <kjnakamura@apu.edu>
2018-07-05 08:03:20 -0700

    Create NewUserInformationVC.swift for the new scene. Move files into more specific folders.

---
+++ Bubble Wrap 2.0/Controllers/Scenes/Authentication/NewUserInformationVC.swift
@@ -0,0 +1,20 @@
+//
+//  NewUserInformationVC.swift
+//  Bubble Wrap 2.0
+//
+//  Created by Kyle Nakamura on 7/5/18.
+//  Copyright © 2018 Kyle Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class NewUserInformationVC: UIViewController {
+    
+    // Outlets
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
