19359dd611f429c6b5b2f593278385963e3b91ec
mrlopez116 <mariolopez@Marios-MacBook-Pro-2.local>
2019-01-03 22:12:39 -0800

    Fixed the scroll view for the create item view

--- Bubble Wrap 2.0/Services/Extensions.swift
+++ Bubble Wrap 2.0/Services/Extensions.swift
@@ -53,3 +53,31 @@ extension CGImagePropertyOrientation {
         }
     }
 }
+
+class DashedBorderView: UIView {
+    
+    let _border = CAShapeLayer()
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    init() {
+        super.init(frame: CGRect.zero)
+        setup()
+    }
+    
+    func setup() {
+        _border.strokeColor = UIColor.white.cgColor
+        _border.fillColor = nil
+        _border.lineDashPattern = [4, 4]
+        self.layer.addSublayer(_border)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        _border.path = UIBezierPath(roundedRect: self.bounds, cornerRadius:8).cgPath
+        _border.frame = self.bounds
+    }
+}
